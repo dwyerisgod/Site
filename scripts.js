46e9770f344e7d7fd20de7deb838d04ffdd06b29
dwyerisgod <kiril.turov.05@icloud.com>
2023-12-14 10:40:54 +0100

    new website

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function loadPage(pageName) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("main-content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", pageName, true);
+    xhttp.send();
+}
