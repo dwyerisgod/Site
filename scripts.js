fd90c1d3bde272a55f72affcbe308521050d1252
dwyerisgod <kiril.turov.05@icloud.com>
2023-12-14 10:56:15 +0100

    update 2

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var pageLinks = document.querySelectorAll('.load-page');
+    pageLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var page = this.getAttribute('data-page');
+            loadPage(page);
+        });
+    });
+});
+
 function loadPage(pageName) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
